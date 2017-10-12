ac284d93f6cd15e4401aab73e0407e302d326ac9
Vsevolod1983 <vgolovko@luxoft.com>
2017-10-12 15:55:17 +0300

    Blender render example has been added: You should pick a color and entire render view will be filled with this color

---
+++ blender_render.py
@@ -0,0 +1,72 @@
+import bpy
+
+bl_info = {"name": "My Render Test Addon", "category": "Render"}
+
+def register():
+    print("Hello World")
+    bpy.utils.register_class(ColorItem)
+    bpy.utils.register_class(UiPanel)
+    bpy.utils.register_class(ExampleRenderEngine)
+
+    bpy.types.Scene.color = bpy.props.PointerProperty(type=ColorItem)
+    
+def unregister():
+    print("Goodbye World")
+    bpy.utils.unregister_class(ExampleRenderEngine)
+    bpy.utils.unregister_class(UiPanel)
+    bpy.utils.unregister_class(ColorItem)
+    del bpy.types.Scene.color
+
+class ColorItem(bpy.types.PropertyGroup):
+    color = bpy.props.FloatVectorProperty(
+                 name = "Color Picker",
+                 subtype = "COLOR",
+                 size = 4,
+                 min = 0.0,
+                 max = 1.0,
+                 default = (0.5,0.5,0.5,1.0)
+                 )
+
+class UiPanel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Render"
+    bl_idname = "OBJECT_NAME"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "render"
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.label(text="Enter Number", icon='WORLD_DATA')
+
+        row = layout.row()
+        row.prop(context.scene.color, "color")
+
+class ExampleRenderEngine(bpy.types.RenderEngine):
+    bl_idname = 'TestRenderer'
+    bl_label = "TestRenderer"
+ 
+#    def __init__(self)
+#    def update(self, data, scene):
+        #if len(context.scene.colors) == 0
+#            context.scene.colors.add()
+#    def render(self, scene)
+
+#    def preview_update(self, context, id)
+#    def preview_render(self)
+
+#    def view_update(self, context)
+#    def view_draw(self, context)
+#    def __del__(self)
+
+    def render(self,scene):
+        sx = scene.render.resolution_x
+        sy = scene.render.resolution_y
+
+        result = self.begin_result(0, 0, sx, sy)
+        layer = result.layers[0].passes["Combined"]      
+        rect = [scene.color.color] * sx * sy
+        layer.rect = rect
+        self.end_result(result)
